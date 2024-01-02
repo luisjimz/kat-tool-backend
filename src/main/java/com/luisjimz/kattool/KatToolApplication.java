78fee744535d755a142f08ec7a831bc32d5053b4
Luis.AdolfoJimenez <luis.adolfojimenez@homeadvisor.com>
2024-01-02 09:58:55 -0500

    Initial commit

---
+++ src/main/java/com/luisjimz/kattool/KatToolApplication.java
@@ -0,0 +1,13 @@
+package com.luisjimz.kattool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisjimz/kattool/KatToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisjimz.kattool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
